3500cd1dcc7ca50795d7b520b66eda2ae415e443
juanoned <juandiego.jd42@gmail.com>
2024-12-05 17:05:05 -0300

    Make Register

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,10 +10,13 @@ import { Stack } from 'expo-router';
 SplashScreen.preventAutoHideAsync();
 
 
+
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
   const [loaded, error] = useFonts({
     Inter:Inter_900Black
   });
